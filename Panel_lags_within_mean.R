9751d41ada2be1c60024418819b602e753e9c541
Tomas Formanek <formanektomas@users.noreply.github.com>
2018-06-24 12:27:25 +0200

    Add lags and within means to panel data

--- Panel_lags_within_mean.R
+++ Panel_lags_within_mean.R
@@ -4,19 +4,21 @@ require(plm)
 require(dplyr)
 #
 #
-# balanced panel
+# Balanced panel, add 1st lags and within mean
 data("Cigar", package = "plm")
 ?Cigar
 #
 Cigar2 <- Cigar %>%  
           group_by(state) %>%
-          mutate(price_1 = lag(price), sales_1 = lag(sales), sales_w = mean(sales))
+          mutate(price_1 = lag(price, k = 1), sales_1 = lag(sales, k = 1), sales_w = mean(sales, na.rm = T))
 #
 #
-# non-balanced panel
+# Non-balanced panel, add 1st lags and within mean
+# .. please note that there is no difference in calclating 
+#    lags and within means for balanced/non-balanced panels
 data("EmplUK", package = "plm")
 ?EmplUK
 #
 EmplUK2 <- EmplUK %>%  
   group_by(firm) %>%
-  mutate(wage_1 = lag(wage), capital_1 = lag(capital), capital_w = mean(capital)) 
+  mutate(wage_1 = lag(wage, k = 1), capital_1 = lag(capital, k = 1), capital_w = mean(capital, na.rm = T)) 
