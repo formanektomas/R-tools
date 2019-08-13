333e7cc90173f30a901d7fe956eca6bd9d1c4793
Tomas Formanek <formanektomas@users.noreply.github.com>
2019-08-13 16:28:41 +0200

    Koroenecker multiplication trivia

--- Kroenecker multiplication trivia.R
+++ Kroenecker multiplication trivia.R
@@ -9,4 +9,8 @@ kronecker(iota4,I3) # I3 matrix stacked 4-times: 4x1 array of I3
 J4 <-  iota4 %*% t(iota4) # 4x4 matrix of ones
 #
 kronecker(I3,J4) # block-diagonal matrix of J4 matrices
-kronecker(J4,I3) # I3 matrix stacked: as 4x4 array
+kronecker(J4,I3) # each element of J4 multiplied by I3 and stacked
+# ..  or, to better understand what `kronecker(J4,I3)` actually does:
+J4a <- matrix(1:16, nrow=4)
+J4a
+kronecker(J4a,I3)
